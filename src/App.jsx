1cd6af9a81fbd194c10664021d0d1ca7ddb8f56e
mohosin2126 <mohosin2126@gmail.com>
2024-12-19 22:09:35 +0600

    code has been updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import  { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import png from '../public/WeatherIcons.gif';
 import Weather from "./components/weather/index.jsx";
 import Location from "./components/location/index.jsx";
@@ -17,21 +17,30 @@ function App() {
     }, []);
 
     return (
-        <div className={`h-[100vh] flex justify-center items-center ${loading ? '' : 'lg:h-[100vh] w-full flex-col lg:flex-row gap-4 py-8 lg:py-0'}`}>
+        <div
+            className={` flex justify-center items-center bg-cover bg-center bg-no-repeat ${
+                loading ? '' : 'lg:h-screen w-full flex-col lg:flex-row gap-4 py-8 lg:py-0'
+            }`}
+            style={{
+                backgroundImage: "url('../public/HD-wallpaper-sky-clouds-sky-clouds-nature.jpg')"
+            }}
+        >
             {loading ? (
-                <div className='w-[350px] flex flex-col items-center bg-white bg-opacity-15 border-2 border-white border-opacity-20 backdrop-blur-lg h-[555px] rounded-2xl p-5 font-sans'>
-                    <img className='w-5/6' src={png} alt='Weather Icons' />
-                    <div className='text-white text-center'>
-                        <h1 className='text-2xl font-semibold'>Detecting your location</h1>
-                        <h2 className='mt-3'>Your current location will be displayed on the App & used for calculating real-time weather.</h2>
-                        <h2 className='mt-3 font-medium'>Please allow your location</h2>
+                <div className="w-[350px] flex flex-col items-center bg-white bg-opacity-15 border-2 border-white border-opacity-20 backdrop-blur-lg h-[555px] rounded-2xl p-5 font-sans">
+                    <img className="w-5/6" src={png} alt="Weather Icons" />
+                    <div className="text-white text-center">
+                        <h1 className="text-2xl font-semibold">Detecting your location</h1>
+                        <h2 className="mt-3">
+                            Your current location will be displayed on the App & used for
+                            calculating real-time weather.
+                        </h2>
+                        <h2 className="mt-3 font-medium">Please allow your location</h2>
                     </div>
                 </div>
             ) : (
-                <div className='lg:h-[100vh] md:font-light w-full flex flex-col lg:flex-row justify-center items-center gap-4 py-8 lg:py-0'>
-                    <Location/>
-                 <Weather/>
-
+                <div className="lg:h-screen md:font-light w-full flex flex-col lg:flex-row justify-center items-center gap-4 py-8 lg:py-0">
+                    <Location />
+                    <Weather />
                 </div>
             )}
         </div>
